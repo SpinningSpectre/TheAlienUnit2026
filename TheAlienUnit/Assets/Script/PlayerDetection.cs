78601f36aa48e46fa6509669ff1acee9a548dc5c
Captain_Void <145536674+Egg-Angel@users.noreply.github.com>
2026-02-01 14:48:08 +0100

    security level ui

--- TheAlienUnit/Assets/Script/PlayerDetection.cs
+++ TheAlienUnit/Assets/Script/PlayerDetection.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,6 +12,7 @@ public class PlayerDetection : MonoBehaviour
     public GameObject maskMiscVisual;
     public GameObject faceCurrentBody;
     public bool currentMaskVoided = false;
+    public TMP_Text SecurityLevelUI;
     [SerializeField] private TilemapCollider2D[] doors;
 
     public GameObject maskPrefab;
@@ -27,6 +29,7 @@ public class PlayerDetection : MonoBehaviour
 
     public void SetMask(int level, Color maskColor)
     {
+        SecurityLevelUI.text = level.ToString();
         currentMaskLevel = level;
         currentMaskVoided = false;
         maskSkinVisual.SetActive(level != 0);
