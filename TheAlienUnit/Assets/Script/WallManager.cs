4bcade5f6e8487bc5f24e8e5b38744532be5587d
Mike Oomen <mike.oomen1@gmail.com>
2026-02-01 02:43:22 +0100

    Added WallManager
    This script will make walls appear when in front of the player and disappear when behind

---
+++ TheAlienUnit/Assets/Script/WallManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WallManager : MonoBehaviour
+{
+    [SerializeField] private Tilemap walls;
+    [SerializeField] private Tilemap visibleWalls;
+    const float RESET_TIMER = 0.125f;
+    float timer = RESET_TIMER;
+    BoundsInt curBounds;
+    
+    Camera cam;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    void LateUpdate()
+    {
+        timer -= Time.deltaTime;
+        if (timer > 0) return;
+
+        BoundsInt camBounds =  GetCamBounds();
+        if (curBounds != null && curBounds == camBounds) return;
+
+        curBounds = camBounds;
+        timer = RESET_TIMER;
+
+        visibleWalls.ClearAllTiles();
+        for (int x = camBounds.min.x; x < camBounds.max.x; x++) { 
+            for (int y = Mathf.FloorToInt(camBounds.center.y); y < camBounds.max.y; y++)
+            {
+                Vector3Int pos = new Vector3Int(x, y);
+                if (walls.GetTile(pos) == null) continue;
+                
+                visibleWalls.SetTile(pos, walls.GetTile(pos));
+                
+            }
+        }
+
+    }
+
+    private BoundsInt GetCamBounds()
+    {
+        Vector3 min = cam.ScreenToWorldPoint(Vector3.zero);
+        Vector3 max = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        Vector3Int minInt = new Vector3Int((int)min.x - 1, (int)min.y);
+        Vector3Int maxInt = new Vector3Int((int)max.x + 1, (int)max.y + 1);
+
+        BoundsInt camBounds = new BoundsInt();
+        camBounds.min = minInt;
+        camBounds.max = maxInt;
+
+        return camBounds;
+    }
+}
